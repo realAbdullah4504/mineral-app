2acd1b8876d618cf184784b264cc9ea88b193d28
realAbdullah4504 <abdullahjaved4504@gmail.com>
2024-07-04 18:30:59 +0500

    tailwind applied

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const Home = ()=> {
+const Home = () => {
   return (
-    <div>Home Page Content</div>
-  )
+    <div className="max-w-[1440px] mx-auto">
+      Home Page Content
+      <div>Slider</div>
+      <div>Welcome portal section</div>
+      <div>Geological section</div>
+      <div>Global miners</div>
+      <div>Mineral Portal</div>
+      <div>News and Events</div>
+      <div>Financial Stats</div>
+    </div>
+  );
 };
 export default Home;
