8466f9fa88074223cc07eec216410c73fd472bcc
HashaamBinEjaz <139843321+HashaamBinEjaz@users.noreply.github.com>
2024-07-05 17:09:45 +0500

    Slider Done + Mineral Potential Section done but need to fix image links

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,6 @@
 import { GeologicalSection, Slider } from "components";
+import { Slider2 } from "components/slider2";
+import { MineralSectorPotential } from "components/MineralSectorPotential";
 import React from "react";
 
 const Home = () => {
@@ -11,6 +13,8 @@ const Home = () => {
       <div>Mineral Portal</div>
       <div>News and Events</div>
       <div>Financial Stats</div>
+      <Slider2 />
+      <MineralSectorPotential />
     </>
   );
 };
