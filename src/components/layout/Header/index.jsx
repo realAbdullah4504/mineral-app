a10daacb664138a638f1df4ef248aaddfa683139
Kame <56468265+Areeb5146@users.noreply.github.com>
2024-07-14 09:46:01 -0700

    added link to Investment

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -81,7 +81,7 @@ const Header = ({ title = "" }) => {
             <div className="header_bottom_menu">
               <Link to="/">Home</Link>
               <ul className="has_sub_menu">
-                <li>
+                <li onClick={() => handleNavigate('/business-investment')}>
                   Investment
                   <ul className="sub_menu">
                     {SubMenuInvestment?.map((data, i) => (
