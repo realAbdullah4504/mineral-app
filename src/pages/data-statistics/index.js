c4d2289a3b83e315431593e42b20ca6e118797a3
HashaamBinEjaz <139843321+HashaamBinEjaz@users.noreply.github.com>
2024-07-22 20:55:54 +0500

    FIxed pages alignment issues

--- src/pages/data-statistics/index.js
+++ src/pages/data-statistics/index.js
@@ -55,22 +55,22 @@ function Csr() {
       }}
     >
       <Container classes="mt-8 w-[90%]">
-        <div className="business-investment-container">
-          <BreadCrumbs breadcrumbs={breadcrumbs} />
-          <IntroContainer
-            title={
-              <h1 className="font-ibm-plex-sans font-semibold text-[64px] p-[0px]">
-                Data & Statistics
-                <hr className="mt-1 w-32 border-4 border-green-500 rounded-full" />
-              </h1>
-            }
-            content="Statistical data related to mining operations."
-            height="70%"
-            width="100%"
-            introHeight="280px"
-          ></IntroContainer>
+        <BreadCrumbs breadcrumbs={breadcrumbs} />
+        <div className="flex flex-wrap mt-[20px]">
+          <div className="flex flex-col">
+            <h1 className="font-ibm-plex-sans font-semibold text-[64px] p-[0px]">
+              Data & Statistics
+              <hr className="mt-1 w-32 border-4 border-green-500 rounded-full" />
+            </h1>
+            <div
+              className="font-helvetica font-normal text-[22px] leading-[24px] mt-4"
+              style={{ whiteSpace: "pre-line" }}
+            >
+              Statistical data related to mining operations.
+            </div>
+          </div>
 
-          <div className="w-full flex justify-center mb-16">
+          <div className="w-full flex justify-center mb-16 mt-20">
             <div className="grid grid-cols-3 w-[70%] gap-4 ">
               {csrCards.map((item) => (
                 <FlipCards
