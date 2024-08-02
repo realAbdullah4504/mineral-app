d579dc7b92e03d5b28299dfd4254a3686da4cfa1
Kame <56468265+Areeb5146@users.noreply.github.com>
2024-08-02 05:42:44 -0700

    debugger and bug fix

--- src/pages/register-professional/index.jsx
+++ src/pages/register-professional/index.jsx
@@ -133,9 +133,9 @@ const RegisterProfessional = () => {
       const companyobj = {
         OrganizationType: "MiningProfessionals",
         OrganizationName: values.OrganizationName,
-        Email: values.Email,
+        Email: user?.Email,
         Name: user?.UserFullName,
-        MobileNumber: values.MobileNumber,
+        MobileNumber: user?.PhoneNumber,
         CNIC: values.CNIC,
         Qualification: values.Qualification,
         DegreeTitle: values.DegreeTitle,
@@ -224,12 +224,12 @@ const RegisterProfessional = () => {
             <div className="grid lg:grid-cols-3 sm:grid-cols-2 gap-10">
               <Form.Item
                 name="Name"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your Contact No.!",
-                  },
-                ]}
+                // rules={[
+                //   {
+                //     required: true,
+                //     message: "Please input your Contact No.!",
+                //   },
+                // ]}
               >
                 <div className="relative mt-2 w-full">
                   <input
@@ -250,12 +250,12 @@ const RegisterProfessional = () => {
               </Form.Item>
               <Form.Item
                 name="MobileNumber"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your Contact No.!",
-                  },
-                ]}
+                // rules={[
+                //   {
+                //     required: true,
+                //     message: "Please input your Contact No.!",
+                //   },
+                // ]}
               >
                 <div className="relative mt-2 w-full">
                   <input
@@ -276,13 +276,13 @@ const RegisterProfessional = () => {
               </Form.Item>
               <Form.Item
                 name="Email"
-                rules={[
-                  {
-                    required: true,
-                    type: "email",
-                    message: "Please input a valid Email!",
-                  },
-                ]}
+                // rules={[
+                //   {
+                //     required: true,
+                //     type: "email",
+                //     message: "Please input a valid Email!",
+                //   },
+                // ]}
               >
                 <div className="relative mt-2 w-full">
                   <input
