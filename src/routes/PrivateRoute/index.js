7163fe9e5805bdd5135c85c579cd969c2bdc5a03
UsmanGhani6217 <bossbig6217@gmail.com>
2024-08-06 22:03:39 +0500

    added expiray check in the private route

--- src/routes/PrivateRoute/index.js
+++ src/routes/PrivateRoute/index.js
@@ -1,10 +1,18 @@
 import { Navigate } from "react-router-dom";
+import { logOut } from "utils/helpers";
+import { jwtDecode } from "jwt-decode";
 import { getCookie } from "services/session/cookies";
 import PropTypes from "prop-types";
 
 const useAuth = () => {
   const accessToken = getCookie("token");
-  return accessToken ? true : false;
+    let isExpired = false;
+    if (accessToken) {
+      const payLoad = jwtDecode(accessToken);
+       isExpired = Date.now() > payLoad.exp * 1000;
+       logOut();
+    }
+  return (accessToken && !isExpired) ? true : false;
 };
 
 export const PrivateRoute = ({ component: Component, layout: Layout }) => {
